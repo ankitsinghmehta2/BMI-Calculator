e3ad8a2adb4b6b1fb940771b6cf91d65001e3d72
Ankit Mehta <126945410+ankitsinghmehta2@users.noreply.github.com>
2024-01-18 18:55:31 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@ function weight() {
     const h=document.querySelector("#height").value
     const w=document.querySelector("#weight").value
     const output1 = document.querySelector(".output")
-    const bmi = (w * 10000 / (h * h)).toFixed(2);
+    const bmi = (w * 10000 / (h * h)).toFixed(2):
 
 if (bmi < 18.6) {
-   output1.innerHTML = `YOUR BMI IS ${bmi}. YOU ARE UNDERWEIGHT`;
+   output1.innerHTML = `YOUR BMI IS ${bmi}. YOU ARE UNDERWEIGHT`;:
     output1.style.fontSize = "30px";
     output1.style.fontWeight = "700";
     output1.style.color="red";
